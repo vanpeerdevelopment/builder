50944dad0b6fe64166e2ab9bc3646067f5a96abb
Development Van Peer <dieter@vanpeerdevelopment.be>
2014-05-20 20:40:20 +0200

    convert ui test project to maven project + adapt test command for travis to include integration tests

---
+++ be.vanpeerdevelopment.eclipse.builder.ui.test/src/be/vanpeerdevelopment/eclipse/builder/ui/FirstTest.java
@@ -0,0 +1,14 @@
+package be.vanpeerdevelopment.eclipse.builder.ui;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class FirstTest {
+
+	@Test
+	public void test() {
+		assertEquals(4, 2 + 2);
+	}
+
+}
