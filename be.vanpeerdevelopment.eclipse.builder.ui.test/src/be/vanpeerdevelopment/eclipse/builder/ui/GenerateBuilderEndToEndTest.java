5d5b137b79e8f129048be7ce5f5494067dabd953
Development Van Peer <dieter@vanpeerdevelopment.be>
2014-05-24 11:10:51 +0200

    create a workbench in the test + set UIHarness to true + enable xvfb on travis

--- be.vanpeerdevelopment.eclipse.builder.ui.test/src/be/vanpeerdevelopment/eclipse/builder/ui/GenerateBuilderEndToEndTest.java
+++ be.vanpeerdevelopment.eclipse.builder.ui.test/src/be/vanpeerdevelopment/eclipse/builder/ui/GenerateBuilderEndToEndTest.java
@@ -2,16 +2,24 @@ package be.vanpeerdevelopment.eclipse.builder.ui;
 
 import static org.junit.Assert.assertEquals;
 
+import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(SWTBotJunit4ClassRunner.class)
 public class GenerateBuilderEndToEndTest {
 
+	protected static SWTWorkbenchBot workbench;
+
+	@BeforeClass
+	public static void beforeTests() {
+		workbench = new SWTWorkbenchBot();
+	}
+
 	@Test
 	public void generateBuilderAddedToSourceMenu() {
 		assertEquals(true, true);
 	}
-
 }
