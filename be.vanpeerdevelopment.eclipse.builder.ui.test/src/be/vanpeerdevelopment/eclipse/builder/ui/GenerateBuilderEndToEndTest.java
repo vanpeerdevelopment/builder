be447becfcded6e9b206bbf417b942de1f0b4a92
Development Van Peer <dieter@vanpeerdevelopment.be>
2014-05-24 10:58:35 +0200

    add ui end to end test + remove first dummy test

---
+++ be.vanpeerdevelopment.eclipse.builder.ui.test/src/be/vanpeerdevelopment/eclipse/builder/ui/GenerateBuilderEndToEndTest.java
@@ -0,0 +1,17 @@
+package be.vanpeerdevelopment.eclipse.builder.ui;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SWTBotJunit4ClassRunner.class)
+public class GenerateBuilderEndToEndTest {
+
+	@Test
+	public void generateBuilderAddedToSourceMenu() {
+		assertEquals(true, true);
+	}
+
+}
