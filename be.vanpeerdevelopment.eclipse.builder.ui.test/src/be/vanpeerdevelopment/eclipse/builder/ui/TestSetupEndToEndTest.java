b61e43a2ed60d17e7dea6ad47c6a817274a4aef2
Development Van Peer <dieter@vanpeerdevelopment.be>
2014-05-26 19:14:41 +0200

    test end to end test setup

--- be.vanpeerdevelopment.eclipse.builder.ui.test/src/be/vanpeerdevelopment/eclipse/builder/ui/TestSetupEndToEndTest.java
+++ be.vanpeerdevelopment.eclipse.builder.ui.test/src/be/vanpeerdevelopment/eclipse/builder/ui/TestSetupEndToEndTest.java
@@ -2,19 +2,21 @@ package be.vanpeerdevelopment.eclipse.builder.ui;
 
 import static org.junit.Assert.assertEquals;
 
+import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
 import org.junit.Test;
 
-public class GenerateBuilderEndToEndTest extends EndToEndTest {
+public class TestSetupEndToEndTest extends EndToEndTest {
 
 	private static final String JAVA_PERSPECTIVE = "Java";
+
+	@Test(expected = WidgetNotFoundException.class)
+	public void welcomeViewIsClosed() {
+		workbench.viewByTitle("Welcome");
+	}
 
 	@Test
 	public void javaPerspectiveOpenByDefault() {
 		assertEquals(JAVA_PERSPECTIVE, workbench.activePerspective().getLabel());
 	}
 
-	@Test
-	public void generateBuilderAddedToSourceMenu() {
-		assertEquals(true, true);
-	}
 }
