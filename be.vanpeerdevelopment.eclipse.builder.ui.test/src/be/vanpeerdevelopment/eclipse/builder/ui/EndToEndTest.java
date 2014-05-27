beab140bc47f749d856cff23b5fc35075e289fde
Development Van Peer <dieter@vanpeerdevelopment.be>
2014-05-27 19:09:18 +0200

    make end to end test setup idempotent

--- be.vanpeerdevelopment.eclipse.builder.ui.test/src/be/vanpeerdevelopment/eclipse/builder/ui/EndToEndTest.java
+++ be.vanpeerdevelopment.eclipse.builder.ui.test/src/be/vanpeerdevelopment/eclipse/builder/ui/EndToEndTest.java
@@ -1,6 +1,7 @@
 package be.vanpeerdevelopment.eclipse.builder.ui;
 
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
@@ -8,17 +9,59 @@ import org.junit.runner.RunWith;
 @RunWith(SWTBotJunit4ClassRunner.class)
 public abstract class EndToEndTest {
 
-	private static final String WELCOME_VIEW_TITLE = "Welcome";
+	protected static final String WELCOME_VIEW_TITLE = "Welcome";
+	protected static final String JAVA_PERSPECTIVE_LABEL = "Java";
 
 	protected static SWTWorkbenchBot workbench;
 
 	@BeforeClass
 	public static void beforeTests() {
 		workbench = new SWTWorkbenchBot();
-		closeWelcomeView();
+		closeWelcomeViewIfNeeded();
+		openJavaPerspectiveIfNeeded();
+	}
+
+	private static void closeWelcomeViewIfNeeded() {
+		if (isWelcomeViewOpen())
+			closeWelcomeView();
+	}
+
+	private static boolean isWelcomeViewOpen() {
+		for (SWTBotView view : workbench.views())
+			if (view.getTitle().equals(WELCOME_VIEW_TITLE))
+				return true;
+		return false;
 	}
 
 	private static void closeWelcomeView() {
-		workbench.viewByTitle(WELCOME_VIEW_TITLE).close();
+		workbench
+				.viewByTitle(WELCOME_VIEW_TITLE)
+				.close();
+	}
+
+	private static void openJavaPerspectiveIfNeeded() {
+		if (!isJavaPerspectiveOpen())
+			openJavaPerspective();
+	}
+
+	private static boolean isJavaPerspectiveOpen() {
+		return workbench.activePerspective().getLabel().equals(JAVA_PERSPECTIVE_LABEL);
+	}
+
+	private static void openJavaPerspective() {
+		workbench
+				.menu("Window")
+				.menu("Open Perspective")
+				.menu("Other...")
+				.click();
+		workbench
+				.shell("Open Perspective")
+				.activate();
+		workbench
+				.table()
+				.select("Java");
+		workbench
+				.button("OK")
+				.click();
 	}
 }
--- be.vanpeerdevelopment.eclipse.builder.ui.test/src/be/vanpeerdevelopment/eclipse/builder/ui/TestSetupEndToEndTest.java
+++ be.vanpeerdevelopment.eclipse.builder.ui.test/src/be/vanpeerdevelopment/eclipse/builder/ui/TestSetupEndToEndTest.java
@@ -7,16 +7,13 @@ import org.junit.Test;
 
 public class TestSetupEndToEndTest extends EndToEndTest {
 
-	private static final String WELCOME_VIEW_TITLE = "Welcome";
-	private static final String JAVA_PERSPECTIVE_LABEL = "Java";
-
 	@Test(expected = WidgetNotFoundException.class)
 	public void welcomeViewIsClosed() {
 		workbench.viewByTitle(WELCOME_VIEW_TITLE);
 	}
 
 	@Test
-	public void javaPerspectiveIsOpenByDefault() {
+	public void javaPerspectiveIsOpen() {
 		assertEquals(JAVA_PERSPECTIVE_LABEL, workbench.activePerspective().getLabel());
 	}
 
