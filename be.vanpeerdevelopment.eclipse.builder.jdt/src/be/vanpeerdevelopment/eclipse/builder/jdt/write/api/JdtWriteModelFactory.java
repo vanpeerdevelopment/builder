77c1530173fae20937d73dc1c5030b51c1c30ee9
Development Van Peer <dieter@vanpeerdevelopment.be>
2014-06-17 22:58:58 +0200

    rename jdt write classes

--- be.vanpeerdevelopment.eclipse.builder.jdt/src/be/vanpeerdevelopment/eclipse/builder/jdt/write/api/JdtWriteModelFactory.java
+++ be.vanpeerdevelopment.eclipse.builder.jdt/src/be/vanpeerdevelopment/eclipse/builder/jdt/write/api/JdtWriteModelFactory.java
@@ -1,10 +1,10 @@
 package be.vanpeerdevelopment.eclipse.builder.jdt.write.api;
 
-import be.vanpeerdevelopment.eclipse.builder.jdt.write.internal.WriteModel;
+import be.vanpeerdevelopment.eclipse.builder.jdt.write.internal.JdtWriteModelImpl;
 
 public class JdtWriteModelFactory {
 
 	public static JdtWriteModel createJdtWriteModel() {
-		return new WriteModel();
+		return new JdtWriteModelImpl();
 	}
 }
--- be.vanpeerdevelopment.eclipse.builder.jdt/src/be/vanpeerdevelopment/eclipse/builder/jdt/write/internal/JdtWriteModelImpl.java
+++ be.vanpeerdevelopment.eclipse.builder.jdt/src/be/vanpeerdevelopment/eclipse/builder/jdt/write/internal/JdtWriteModelImpl.java
@@ -2,6 +2,6 @@ package be.vanpeerdevelopment.eclipse.builder.jdt.write.internal;
 
 import be.vanpeerdevelopment.eclipse.builder.jdt.write.api.JdtWriteModel;
 
-public class WriteModel implements JdtWriteModel {
+public class JdtWriteModelImpl implements JdtWriteModel {
 
 }
