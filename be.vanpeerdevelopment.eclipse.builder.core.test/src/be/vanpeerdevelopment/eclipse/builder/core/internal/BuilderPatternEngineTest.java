2f0d0ab7da0713f610c3049f067e49816e591d24
Development Van Peer <dieter@vanpeerdevelopment.be>
2014-06-25 21:57:08 +0200

    test BuilderPatternEngine

---
+++ be.vanpeerdevelopment.eclipse.builder.core.test/src/be/vanpeerdevelopment/eclipse/builder/core/internal/BuilderPatternEngineTest.java
@@ -0,0 +1,43 @@
+package be.vanpeerdevelopment.eclipse.builder.core.internal;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import org.eclipse.core.runtime.IPath;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import be.vanpeerdevelopment.eclipse.builder.core.UnitTest;
+import be.vanpeerdevelopment.eclipse.builder.jdt.read.api.JdtReadModel;
+import be.vanpeerdevelopment.eclipse.builder.jdt.read.api.ReadableCompilationUnit;
+import be.vanpeerdevelopment.eclipse.builder.jdt.read.api.ReadableType;
+import be.vanpeerdevelopment.eclipse.builder.jdt.write.api.WriteableCompilationUnit;
+
+public class BuilderPatternEngineTest extends UnitTest {
+
+	private static final String TYPE_NAME = "Person";
+
+	@Mock
+	private JdtReadModel jdtReadModel;
+	@InjectMocks
+	private BuilderPatternEngine builderPatternEngine;
+
+	@Mock
+	private IPath compilationUnitLocation;
+	@Mock
+	private ReadableCompilationUnit compilationUnit;
+	@Mock
+	private ReadableType type;
+
+	@Test
+	public void generateBuilder() {
+		when(jdtReadModel.getCompilationUnit(compilationUnitLocation)).thenReturn(compilationUnit);
+		when(compilationUnit.getOnlyType()).thenReturn(type);
+		when(type.getSimpleName()).thenReturn(TYPE_NAME);
+
+		WriteableCompilationUnit builder = builderPatternEngine.generateBuilder(compilationUnitLocation);
+
+		assertThat(builder.getName()).isEqualTo(TYPE_NAME + "Builder");
+	}
+}
