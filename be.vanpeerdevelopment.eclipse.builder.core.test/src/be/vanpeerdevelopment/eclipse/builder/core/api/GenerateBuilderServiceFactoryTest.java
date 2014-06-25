5d4af18a3dc389ab527b4d069ee34c1e16b161a8
Development Van Peer <dieter@vanpeerdevelopment.be>
2014-06-25 21:00:37 +0200

    added builder core test plug-in project

---
+++ be.vanpeerdevelopment.eclipse.builder.core.test/src/be/vanpeerdevelopment/eclipse/builder/core/api/GenerateBuilderServiceFactoryTest.java
@@ -0,0 +1,15 @@
+package be.vanpeerdevelopment.eclipse.builder.core.api;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.junit.Test;
+
+import be.vanpeerdevelopment.eclipse.builder.core.UnitTest;
+
+public class GenerateBuilderServiceFactoryTest extends UnitTest {
+
+	@Test
+	public void test() {
+		assertThat(true).isTrue();
+	}
+}
