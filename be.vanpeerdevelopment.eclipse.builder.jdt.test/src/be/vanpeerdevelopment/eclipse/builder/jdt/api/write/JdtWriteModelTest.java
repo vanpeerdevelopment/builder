77201f1460c0356c6a3788295b914c56777deb9e
Development Van Peer <dieter@vanpeerdevelopment.be>
2014-09-14 16:47:09 +0200

    added test for JdtWriteModel

---
+++ be.vanpeerdevelopment.eclipse.builder.jdt.test/src/be/vanpeerdevelopment/eclipse/builder/jdt/api/write/JdtWriteModelTest.java
@@ -0,0 +1,40 @@
+package be.vanpeerdevelopment.eclipse.builder.jdt.api.write;
+
+import static be.vanpeerdevelopment.eclipse.builder.jdt.api.write.command.CreateCompilationUnitCommandTestBuilder.aCreateCompilationUnitCommand;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.eclipse.core.runtime.IPath;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import be.vanpeerdevelopment.eclipse.builder.jdt.UnitTest;
+import be.vanpeerdevelopment.eclipse.builder.jdt.api.write.command.CreateCompilationUnitCommand;
+import be.vanpeerdevelopment.eclipse.builder.jdt.internal.common.Workspace;
+import be.vanpeerdevelopment.eclipse.builder.jdt.internal.write.WriteablePackageFragment;
+
+public class JdtWriteModelTest extends UnitTest {
+
+	@Mock
+	private Workspace workspace;
+	@InjectMocks
+	private JdtWriteModel jdtWriteModel;
+
+	@Mock
+	private IPath packageLocation;
+	@Mock
+	private WriteablePackageFragment writeablePackageFragment;
+
+	@Test
+	public void createCompilationUnit() {
+		CreateCompilationUnitCommand command = aCreateCompilationUnitCommand()
+				.withPackageLocation(packageLocation)
+				.build();
+		when(workspace.getPackage(packageLocation)).thenReturn(writeablePackageFragment);
+
+		jdtWriteModel.createCompilationUnit(command);
+
+		verify(writeablePackageFragment).createCompilationUnit(command);
+	}
+}
